c4751be4c80c5208e886c93a138c1ad8cf809d70
kowalsking <kovvalsking@gmail.com>
2023-02-04 23:26:51 +0200

    Lesson 19

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
-let info: {
-  officeId: number;
-  isOpened: boolean;
-  contacts: {
-    phone: string;
-    email: string;
-    address: {
-      city: string;
-    }
-  }
+type RequestType = {
+  topicId: number;
+  status?: 'published' | 'draft' | 'deleted';
+}
+
+type ResponseT = {
+  question: string;
+  answer: string;
+  tags: string[];
+  likes: number;
+  status: 'published' | 'draft' | 'deleted'
+}[]
+
+async function getFaqs(req: RequestType): Promise<ResponseT> {
+  const res = await fetch('/faqs', {
+    method: 'POST',
+    body: JSON.stringify(req)
+  });
+  const data: ResponseT = await res.json();
+  return data;
 }
