b8613088640ab45437d394bef4690301a5b1b33e
kowalsking <kovvalsking@gmail.com>
2023-02-09 22:04:09 +0200

    Method overload. My version

--- app.ts
+++ app.ts
@@ -1,39 +1,12 @@
-enum PaymentStatus {
-  Hold,
-  Processed,
-  Reversed
-}
+class User {
+  skills!: string[];
 
-class Payment {
-  id: number;
-  status: PaymentStatus;
-  createdAt: Date;
-  updatedAt!: Date;
-
-
-  constructor(id: number) {
-    this.id = id;
-    this.createdAt = new Date();
-    this.status = PaymentStatus.Hold;
-  }
-
-  getPaymentLifeTime(): number {
-    return new Date().getTime() - this.createdAt.getTime();
-  }
-
-  unholdPayment() {
-    if (this.status === PaymentStatus.Processed) {
-      throw new Error('Payment cant be returned')
+  addSkill(skills: string[]): void
+  addSkill(skills: string): void
+  addSkill(skills: string | string[]): void {
+    if (Array.isArray(skills)) {
+      this.skills = skills;
     }
-
-    this.status = PaymentStatus.Reversed;
-    this.updatedAt = new Date();
   }
+
 }
-
-const payment = new Payment(1);
-payment.unholdPayment();
-console.log(payment)
-const time = payment.getPaymentLifeTime();
-
-console.log(time)
