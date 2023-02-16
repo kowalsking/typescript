5266f27271e0eb988c768210cb348a9068039a48
kowalsking <kovvalsking@gmail.com>
2023-02-16 22:22:15 +0200

    Lesson 61. Generics

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log()
+const a: Array<number> = [1, 2, 3]
+
+async function test() {
+  const a = await new Promise<number>((resolve, reject) => {
+    resolve(1)
+  })
+}
+
+const check: Record<string, boolean> = {
+  drive: true,
+  kpp: false,
+}
+ 
