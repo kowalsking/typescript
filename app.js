84d215a85950f35a77541764956ef61f6a29048c
kowalsking <kovvalsking@gmail.com>
2023-02-19 20:23:38 +0200

    Mixins app.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 "use strict";
-const data = [
-    { id: 1, name: 'One' },
-    { id: 3, name: 'Two' },
-    { id: 2, name: 'Three' },
-];
-function filterArr(arr, to = 'up') {
-    if (to === 'up') {
-        return arr.sort((a, b) => a.id - b.id);
+class List {
+    constructor(items) {
+        this.items = items;
     }
-    return arr.sort((a, b) => b.id - a.id);
 }
-console.log(filterArr(data, 'up'));
-console.log(filterArr(data, 'down'));
+class ExtendedListClass extends List {
+    first() {
+        return this.items[0];
+    }
+}
+function ExtendedList(Base) {
+    return class ExtendedList extends Base {
+        first() {
+            return this.items[0];
+        }
+    };
+}
+const list = ExtendedList(List);
+const result = new list(['1', '2']);
+console.log(result.first());
