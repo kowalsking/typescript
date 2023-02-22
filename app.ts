7f6c8b8ef4ee22553c76c65c3f5d8a439dfc0609
kowalsking <kovvalsking@gmail.com>
2023-02-22 10:26:59 +0200

    Group ex. by course

--- app.ts
+++ app.ts
@@ -10,14 +10,37 @@ const data: Data[] = [
   { group: 4, name: 'a' },
 ]
 
-function group<T, K extends keyof T>(data: T[], key: K) {
-  const result = {}
-  data.forEach(element => {
-    const temp = element[key] as unknown as string;
-    result[temp] ??= []
-    result[temp].push(element)
-  })
-  return result
+// function group<T, K extends keyof T>(data: T[], key: K) {
+//   const result = {}
+//   data.forEach(element => {
+//     const temp = element[key] as unknown as string;
+//     result[temp] ??= []
+//     result[temp].push(element)
+//   })
+//   return result
+// }
+
+// console.log(group(data, 'name'))
+
+interface IGroup<T> {
+  [key: string]: T[]
 }
 
-console.log(group(data, 'name'))
+type key = string | number | symbol
+
+function group<T extends Record<key, any>>(
+  array: T[],
+  key: keyof T
+): IGroup<T> {
+  return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+    const itemKey = item[key]
+    let current = map[itemKey]
+    if (Array.isArray(current)) {
+      current.push(item)
+    } else {
+      current = [item]
+    }
+    map[itemKey] = current
+    return map
+  }, {})
+}
