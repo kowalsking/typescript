faa40049da924f5ea0a07912d4113291bc2f326d
kowalsking <kovvalsking@gmail.com>
2023-02-23 20:44:54 +0200

    Lesson 73. Indexed Access Types

--- app.ts
+++ app.ts
@@ -1,46 +1,35 @@
-interface Data {
-  group: number
+interface Role {
   name: string
 }
 
-const data: Data[] = [
-  { group: 1, name: 'a' },
-  { group: 1, name: 'b' },
-  { group: 3, name: 'c' },
-  { group: 4, name: 'a' },
-]
 
-// function group<T, K extends keyof T>(data: T[], key: K) {
-//   const result = {}
-//   data.forEach(element => {
-//     const temp = element[key] as unknown as string;
-//     result[temp] ??= []
-//     result[temp].push(element)
-//   })
-//   return result
-// }
-
-// console.log(group(data, 'name'))
-
-interface IGroup<T> {
-  [key: string]: T[]
+interface Permission {
+  endDate: Date
 }
 
-type key = string | number | symbol
+interface User {
+  name: string
+  roles: Role[]
+  permission: Permission
+}
 
-function group<T extends Record<key, any>>(
-  array: T[],
-  key: keyof T
-): IGroup<T> {
-  return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
-    const itemKey = item[key]
-    let current = map[itemKey]
-    if (Array.isArray(current)) {
-      current.push(item)
-    } else {
-      current = [item]
-    }
-    map[itemKey] = current
-    return map
-  }, {})
-} 
+const user: User = {
+  name: 'John',
+  roles: [],
+  permission: {
+    endDate: new Date()
+  }
+}
+
+const nameUser = user['name']
+const roleName = 'roles'
+
+type rolesType = User['roles']
+type rolesType1 = User[typeof roleName]
+
+type roleType = User['roles'][number] // Отримати елемент масиву.
+type dateType = User['permission']['endDate'] // Отримати елемент масиву.
+
+const roles = ['admin', 'user', 'superuser'] as const //  roles: readonly ["admin", "user", "superuser"]
+
+type typeRoles = typeof roles[number] //  roles: readonly ["admin", "user", "superuser"]
