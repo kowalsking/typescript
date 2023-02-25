1a70f36cc8d1e8777e66d5e088e736a8ba4c66f6
kowalsking <kovvalsking@gmail.com>
2023-02-26 00:00:57 +0200

    Lesson 78. Template Literal Types

--- app.ts
+++ app.ts
@@ -1,57 +1,17 @@
-type Modifier = 'read' | 'write' | 'update'
+type ReadOrWrite = 'read' | 'write'
+type Bulk = 'bulk' | ''
 
-type UserRoles = {
-  customers?: Modifier
-  projects?: Modifier
-  adminPanel?: Modifier
+type Access = `can${Capitalize<ReadOrWrite>}${Capitalize<Bulk>}`
+
+type ReadOrWriteBulk<T> = T extends `can${infer R}` ? R : never
+type T = ReadOrWriteBulk<Access>
+
+type ErrorOrSuccess = 'error' | 'success'
+
+type ResponseT = {
+  result: `http${Capitalize<ErrorOrSuccess>}`
 }
 
-type ModifierToAccess<Type> = {
-  +readonly [Property in keyof Type as Exclude<
-    `canAccess${string & Property}`,
-    'canAccessadminPanel'
-  >]-?: boolean
-}
-
-type UserAccess2 = ModifierToAccess<UserRoles>
-
-type UserAccess1 = {
-  customers?: boolean
-  projects?: boolean
-  adminPanel: boolean
-}
-
-/// //// /// /// /// /// // /// // /// // // // // // // // // // // // // // // // // //
-
-interface IForm {
-  name: string
-  password: string
-}
-
-const form: IForm = {
-  name: 'John',
-  password: '123',
-}
-
-type IsValid = {
-  isValid: boolean
-  errorMessage?: string
-}
-
-type ModifierTypeForm<Type> = {
-  [Property in keyof Type]-?: IsValid
-}
-
-const formValidation: Validation<IForm> = {
-  name: { isValid: true },
-  password: { isValid: false, errorMessage: 'Password too short' },
-}
-
-type Validation<T> = {
-  [K in keyof T]: {
-    isValid: true
-  } | {
-    isValid: false
-    errorMessage: string
-  }
+const a: ResponseT = {
+  result: 'httpSuccess'
 }
