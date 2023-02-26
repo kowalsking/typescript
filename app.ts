b5975ca5a64a1b4cf8355ade5b4ea1a550547542
kowalsking <kovvalsking@gmail.com>
2023-02-26 20:53:26 +0200

    Lesson 83. Awaited

--- app.ts
+++ app.ts
@@ -1,18 +1,17 @@
-class User {
-  constructor(public id: number, public name: string) {}
+type A = Awaited<Promise<string>>
+type A2 = Awaited<Promise<Promise<string>>>
+
+interface IMenu {
+  name: string
+  url: string
 }
 
-function getData(id: number): User {
-  return new User(id, 'John')
+async function getMenu(): Promise<IMenu[]> {
+  return [{ name: 'Analitics', url: 'http://analytics' }]
 }
 
-type RT = ReturnType<typeof getData> // User
-type RT1 = ReturnType<() => void> // void
-type RT2 = ReturnType<<T>() => T> // unknown
-type RT3 = ReturnType<<T extends string>() => T> // string
+type R = Awaited<ReturnType<typeof getMenu>>
 
-type PT = Parameters<typeof getData> // [id: number]
-
-type first = PT[0] // number
-
-type CP = ConstructorParameters<typeof User> // [id: number, name: string]
+async function getArray<T>(x: T): Promise<Awaited<T>[]> {
+  return [await x]
+}
