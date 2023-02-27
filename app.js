58771d9e8cb59544a3ee128c6da166f2b4b29870
kowalsking <kovvalsking@gmail.com>
2023-02-27 20:33:56 +0200

    Lesson 85. Pattern Decorator

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+class UserService {
+    constructor() {
+        this.users = 1000;
+    }
+    getUsersInDatabase() {
+        return this.users;
+    }
+}
+function nullUser(obj) {
+    obj.users = 0;
+    return obj;
+}
+function logUsers(obj) {
+    console.log('Users: ', obj.users);
+    return obj;
+}
+console.log(new UserService().getUsersInDatabase());
+console.log(nullUser(new UserService()).getUsersInDatabase());
+console.log(logUsers(nullUser(new UserService())).getUsersInDatabase());
---
+++ app.ts
@@ -0,0 +1,26 @@
+interface IUserService {
+  users: number
+  getUsersInDatabase(): number
+}
+
+class UserService implements IUserService {
+  users: number = 1000
+
+  getUsersInDatabase(): number {
+    return this.users
+  }
+}
+
+function nullUser(obj: IUserService) {
+  obj.users = 0
+  return obj
+}
+
+function logUsers(obj: IUserService) {
+  console.log('Users: ', obj.users)
+  return obj
+}
+
+console.log(new UserService().getUsersInDatabase())
+console.log(nullUser(new UserService()).getUsersInDatabase())
+console.log(logUsers(nullUser(new UserService())).getUsersInDatabase())
