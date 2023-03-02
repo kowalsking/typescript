0109686bd01fc367381c5d7cdc40b51b4fcab31f
kowalsking <kovvalsking@gmail.com>
2023-03-02 21:42:34 +0200

    Lesson 93. Generator of parameter

--- app.ts
+++ app.ts
@@ -1,64 +1,28 @@
 interface IUserService {
-  users: number
+  users?: number
   getUsersInDatabase(): number
 }
 
 class UserService implements IUserService {
   private _users!: number
 
-  set users(number) {
-    this._users = number
-  }
-
-  @Log()
-  get users() {
+  getUsersInDatabase(): number {
     return this._users
   }
 
-  getUsersInDatabase(): number {
-    throw new Error('Errrr')
+  setUsersInDatabase(@Positive() num: number): void {
+    this._users = num
   }
 }
 
-function Log() {
+function Positive() {
   return (
     target: Object,
     propertyKey: string | symbol,
-    descriptor: PropertyDescriptor
+    parameterIndex: number
   ) => {
-    const set = descriptor.set;
-    descriptor.set = (...args: any[]) => {
-      console.log(args);
-      // @ts-ignore
-      set?.apply(target, args);
-    }
-  }
-}
-
-function Catch(rethrow: boolean = false) {
-  return (
-    target: Object,
-    _: string | symbol,
-    descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
-  ): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
-    const method = descriptor.value
-
-    descriptor.value = async (...args: any[]) => {
-      try {
-        return await method?.apply(target, args)
-      } catch (e) {
-        if (e instanceof Error) {
-          console.log(e.message)
-
-          if (rethrow) {
-            throw e
-          }
-        }
-      }
-    }
+    
   }
 }
 
 const us = new UserService()
-us.users = 1
-console.log(us.users)
