b26d120f84d1bcc42ce0abfa895e420a3146ec31
kowalsking <kovvalsking@gmail.com>
2023-03-03 23:57:58 +0200

    Play with decorators. Completely override construc

---
+++ app.ts
@@ -0,0 +1,16 @@
+@sealed
+class Person {
+  constructor() {
+    console.log('Person')
+  }
+}
+
+function sealed(target: Function): any {
+
+  target = function() {
+    console.log('DECORATORRR', target)
+  }
+  return target
+}
+
+new Person()
